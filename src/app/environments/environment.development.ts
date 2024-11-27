2781a02d35f482623f023eaf71e7704787bf92b8
jowel <jowelpana44@gmail.com>
2024-11-28 06:21:57 +0800

    updated

---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  projectId: 'fintrack-75b03',
+  appId: '1:789761073986:web:9a4ce2f5d1c09ffa6b9edb',
+  storageBucket: 'fintrack-75b03.appspot.com',
+  apiKey: 'AIzaSyDPzjwdtuWyVuWcBKi57t9wgMewy3Sj0x8',
+  authDomain: 'fintrack-75b03.firebaseapp.com',
+  messagingSenderId: '789761073986',
+};
